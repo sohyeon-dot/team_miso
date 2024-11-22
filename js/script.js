0f8fdc29b460f702d433009bbf96ecf5c666e680
sohyeon-dot <lsjwljh2@naver.com>
2024-11-23 00:54:12 +0900

    메인 오류 최종 수정

--- js/script.js
+++ js/script.js
@@ -137,45 +137,13 @@ $(document).ready(function(){
     $(this).next().stop().slideToggle();
   });
 
-  var cachedWidth = $(window).width();
+  lastWidth = window.innerWidth;
   $(window).resize(function(){
-    var newWidth = $(window).width();
-    if(newWidth !== cachedWidth){
-
-      cachedWidth = newWidth;
-    }
-  });
-
-  var cachedWidth = $(window).width();
-  $(window).resize(function(){
-    var newWidth = $(window).width();
-    if(newWidth !== cachedWidth){
-
-    var delay = 300;
-    var re_timer = null;
-    $(window).on('resize', function(){
-      clearTimeout(re_timer);
-      re_timer = setTimeout(function(){
-      document.location.reload();
-      }, delay);
-    });
-      cachedWidth = newWidth;
-    }
-  });
-
-  var dwidth = jQuery(window).width();
-  jQuery(window).bind('resize', function(e){
-
-    var wwidth = jQuery(window).width();
-
-    if(dwidth!==wwidth){
-      dwidth = jQuery(window).width();
-
-    if (window.RT) clearTimeout(window.RT);
-      window.RT = setTimeout(function(){
-        this.location.reload(false); 
-      }, 1000);
-    }
+  if(window.innerWidth != lastWidth){
+    location.reload();
+    scrollTrigger.refresh();
+  }
+  lastWidth = window.innerWidth;
   });
 
 });
